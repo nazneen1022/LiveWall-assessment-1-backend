84c59a4ef4509141bfb35615cbb7f8d290991f2e
nazneen1022 <nazneen1022@gmail.com>
2020-09-08 16:25:17 +0200

    generated seeds for test data

---
+++ seeders/20200908141404-some-photos.js
@@ -0,0 +1,51 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "photos",
+      [
+        {
+          title: "Test title",
+          description: "test data description",
+          location: "",
+          tags: "test",
+          category: "test",
+          imageUrl:
+            "https://images.unsplash.com/photo-1495567720989-cebdbdd97913?ixlib=rb-1.2.1&w=1000&q=80",
+          feedback: "Y",
+          tips: "N",
+          allowUse: "N",
+          allowDownload: "Y",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Cool Beach",
+          description: "Hot Summer",
+          location: "Goa",
+          tags: "summer",
+          category: "test",
+          imageUrl:
+            "https://pix10.agoda.net/geo/city/18720/1_18720_02.jpg?s=1920x822",
+          feedback: "N",
+          tips: "Y",
+          allowUse: "N",
+          allowDownload: "Y",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
